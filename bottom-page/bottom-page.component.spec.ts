a57a0d539277c0f821f4e775903bec675984b0b3
Thibault JP <thibault60000@gmail.com>
2018-05-30 16:46:33 +0200

    ADd Bottom

---
+++ bottom-page/bottom-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BottomPageComponent } from './bottom-page.component';
+
+describe('BottomPageComponent', () => {
+  let component: BottomPageComponent;
+  let fixture: ComponentFixture<BottomPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BottomPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BottomPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
